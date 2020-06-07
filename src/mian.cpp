4520a8b1780a87828da5800e5e64a37fb884e57a
1827010 <43464024+1827010@users.noreply.github.com>
2020-06-07 20:38:26 +0900

    Update mian.cpp

--- src/mian.cpp
+++ src/mian.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
 #pragma warning(disable : 4996)
 #ifdef this_is_a_great_codo
 問題：1から順番に数字を発言するのだけれども、その数字が3で割り切れれはFizzと発言して、5ならBuzz、１５でならFizzBuzzと発言する
